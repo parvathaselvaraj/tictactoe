e863c819b953dba291160a076835c389a07aa2e9
Paaru13 <parvatha33@gmail.com>
2022-05-12 16:56:38 +0530

    Initial requirements spec

---
+++ spec/tictactoe_spec.rb
@@ -0,0 +1,14 @@
+describe 'TicTactoe' do
+  it 'should have nine cells' do
+  end
+  it 'should have empty board at start' do
+  end
+  it 'should allow two players to play' do
+  end
+  it 'should have first player\'s symbol as "X"' do
+  end
+  it 'should have second player\'s symbol as "O"' do
+  end
+  it 'should allow players to play alternatively' do
+  end
+end
